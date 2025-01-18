d1a87cd930ccbfa1766ef6fe59e184767f8e463e
Daniil Baturyn <notfeardan@gmail.com>
2025-01-18 01:44:59 -0600

    Rebuilt Arrays

--- script.js
+++ script.js
@@ -78,8 +78,29 @@ function updateDOM() {
   onHoldListArray.forEach((onHoldItem, index) => {
     createItemEl(onHoldList, 0, onHoldItem, index);
   });
+  updateOnLoad = true;
+  updateSavedColumns();
 }
 
+function rebuildArrays() {
+  backlogListArray = [];
+  for (let i = 0; i < backlogList.children.length; i++) {
+    backlogListArray.push(backlogList.children[i].textContent);
+  }
+  progressListArray = [];
+  for (let i = 0; i < progressList.children.length; i++) {
+    progressListArray.push(progressList.children[i].textContent);
+  }
+  completeListArray = [];
+  for (let i = 0; i < completeList.children.length; i++) {
+    completeListArray.push(completeList.children[i].textContent);
+  }
+  onHoldListArray = [];
+  for (let i = 0; i < onHoldList.children.length; i++) {
+    onHoldListArray.push(onHoldList.children[i].textContent);
+  }
+  updateDOM();
+}
 function drag(e) {
   draggedItem = e.target;
 }
@@ -100,6 +121,7 @@ function drop(e) {
   });
   const parent = listColumns[currentColumn];
   parent.appendChild(draggedItem);
+  rebuildArrays();
 }
 
 updateDOM();
